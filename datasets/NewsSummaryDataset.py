eaf4285127619ae70a59f45a7864e36885e1af2b
Egor Dmitriev <egordmitriev2@gmail.com>
2017-10-12 23:39:09 +0200

    Updated dataset processing

--- datasets/NewsSummaryDataset.py
+++ datasets/NewsSummaryDataset.py
@@ -1,8 +1,21 @@
 import pandas as pd
 
-df = pd.read_csv("../data/news_summary.csv", encoding="latin1")
-df.drop_duplicates(subset=["ctext"], inplace=True)
-df.dropna(inplace=True)
-df.drop(['author', 'date', 'headlines', 'read_more'], 1, inplace=True)
-df.reset_index(drop=True,inplace=True)
-print(df.describe())
+from datasets.text_utils import clean_text
+
+
+def get_data():
+    df = pd.read_csv("data/news_summary.csv", encoding="latin1")
+    df.drop_duplicates(subset=["ctext"], inplace=True)
+    df.dropna(inplace=True)
+    df.drop(['author', 'date', 'headlines', 'read_more'], 1, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+
+    clean_summaries = []
+    for summary in df.text:
+        clean_summaries.append(clean_text(summary, remove_stopwords=False))
+
+    clean_texts = []
+    for text in df.ctext:
+        clean_texts.append(clean_text(text))
+
+    return clean_texts, clean_summaries
