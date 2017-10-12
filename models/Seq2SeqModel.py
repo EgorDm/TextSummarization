b70b7bd6563915a3dc84b54525191c8c9ab772a5
Egor Dmitriev <egordmitriev2@gmail.com>
2017-10-12 23:39:58 +0200

    Removed kprob constant

--- models/Seq2SeqModel.py
+++ models/Seq2SeqModel.py
@@ -6,20 +6,18 @@ from tensorflow.python.ops.rnn_cell_impl import _zero_state_tensors
 
 
 class Seq2SeqModel:
-    def __init__(self, embedding_matrix, batch_size, vocab_length, vocab_dictionary, cell_size=512, num_layers=2,
-                 kprob=0.8) -> None:
+    def __init__(self, batcher, cell_size=256, num_layers=2):
         # Constants
         self.cell_size = cell_size
         self.num_layers = num_layers
-        self.kprob = kprob
-        self.batch_size = batch_size
-        self.vocab_length = vocab_length
-        self.vocab_dictionary = vocab_dictionary
+        self.batch_size = batcher.batch_size
+        self.vocab_length = len(batcher.vocab_id)
+        self.vocab_dictionary = batcher.vocab_id
 
         self.lr = tf.placeholder(tf.float32, name='learning_rate')
         self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
-        self.input_data = tf.placeholder(tf.int32, [None, None], name='input')
+        self.inputs = tf.placeholder(tf.int32, [None, None], name='input')
         self.targets = tf.placeholder(tf.int32, [None, None], name='targets')
 
         self.in_length = tf.placeholder(tf.int32, (None,), name='in_length')
@@ -27,17 +25,18 @@ class Seq2SeqModel:
         self.out_max_length = tf.reduce_max(self.out_length, name='out_max_length')
 
         # Embeddings
-        self.embeddings = embedding_matrix
+        self.embeddings = batcher.embeddings
 
         # Encoder
-        encoder_embed_input = tf.nn.embedding_lookup(self.embeddings, tf.reverse(self.input_data, [-1]))
+        encoder_embed_input = tf.nn.embedding_lookup(self.embeddings, tf.reverse(self.inputs, [-1]))
         encoder_output, encode_state = self.encoding_layer(encoder_embed_input)
 
         # Decoder
         decoder_input = self.process_encoding_input(self.targets, self.batch_size)
         decoder_embed_input = tf.nn.embedding_lookup(self.embeddings, decoder_input)
 
-        self.train_logits, self.inference_logits = self.decoding_layer(decoder_embed_input, encoder_output, encode_state)
+        self.train_logits, self.inference_logits = self.decoding_layer(decoder_embed_input, encoder_output,
+                                                                       encode_state)
 
         self.train_logits = tf.identity(self.train_logits.rnn_output, 'logits')
         self.inference_logits = tf.identity(self.inference_logits.sample_id, name='predictions')
@@ -67,10 +66,10 @@ class Seq2SeqModel:
         for i in range(self.num_layers):
             with tf.variable_scope('encoder_{}'.format(i)):
                 cell_fw = rnn.LSTMCell(self.cell_size, initializer=tf.random_uniform_initializer(-0.1, 0.1, seed=2))
-                cell_fw = rnn.DropoutWrapper(cell_fw, input_keep_prob=self.kprob)
+                cell_fw = rnn.DropoutWrapper(cell_fw, input_keep_prob=self.keep_prob)
 
                 cell_bw = rnn.LSTMCell(self.cell_size, initializer=tf.random_uniform_initializer(-0.1, 0.1, seed=2))
-                cell_bw = rnn.DropoutWrapper(cell_bw, input_keep_prob=self.kprob)
+                cell_bw = rnn.DropoutWrapper(cell_bw, input_keep_prob=self.keep_prob)
 
                 output, state = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, input, self.in_length,
                                                                 dtype=tf.float32)
@@ -119,5 +118,5 @@ class Seq2SeqModel:
         helper = seq2seq.GreedyEmbeddingHelper(embeddings, start_tokens, end_token)
         decoder = seq2seq.BasicDecoder(decoder_cell, helper, initial_state, output_layer)
 
-        logits, _ = seq2seq.dynamic_decode(decoder, False, True,maximum_iterations=self.out_max_length)
+        logits, _ = seq2seq.dynamic_decode(decoder, False, True, maximum_iterations=self.out_max_length)
         return logits
